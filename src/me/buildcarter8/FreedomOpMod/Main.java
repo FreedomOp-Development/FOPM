63ec2f666fef03ebdbc741dda28ededb4d586926
Tyler <buildcarter8@users.noreply.github.com>
2016-11-06 12:13:49 -0500

    Basic stuff

--- src/me/buildcarter8/FreedomOpMod/Main.java
+++ src/me/buildcarter8/FreedomOpMod/Main.java
@@ -1,1 +1,20 @@
-//TODO:Add Code for main java
+package me.buildcarter8.FreedomOpMod;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+	 public static String plugin_file = "plugin.yml";
+	    public static Main plugin;
+	    public static final String CONFIG_FILE = "config.yml";
+	    @Override
+	    public void onEnable()
+	    {
+	    	
+	    }
+	    @Override
+	    public void onDisable()
+	    {
+	    	
+	    }
+}
