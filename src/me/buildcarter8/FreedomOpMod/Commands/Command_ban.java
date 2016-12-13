faaa70de29f7d08f272e694607f342d841bfaba7
Paul Reilly <imextremelyvexed@gmail.com>
2016-12-12 19:27:52 -0500

    Updated everything to the Apache 2.0 License

--- src/me/buildcarter8/FreedomOpMod/Commands/Command_ban.java
+++ src/me/buildcarter8/FreedomOpMod/Commands/Command_ban.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 FreedomOp Development.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package me.buildcarter8.FreedomOpMod.Commands;
 
 import me.buildcarter8.FreedomOpMod.*;
@@ -7,8 +23,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
-@CommandParameters(name="gtfo", description="Bans a player.", usage="/<command> <player> [reason]", source=SourceType.BOTH, level=AdminLevel.SUPER)
-public class Command_gtfo {
+@CommandParameters(name="ban", description="Bans a player.", usage="/<command> <player> [reason]", source=SourceType.BOTH, level=AdminLevel.SUPER)
+public class Command_ban {
     public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
         if (args.length > 2) {
             Player t = Bukkit.getPlayer(args[0]);
--- src/me/buildcarter8/FreedomOpMod/Commands/Command_cookie.java
+++ src/me/buildcarter8/FreedomOpMod/Commands/Command_cookie.java
@@ -13,22 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package me.buildcarter8.FreedomOpMod.Commands;
 
-package me.buildcarter8.FreedomOpMod.Banning;
-
-import java.io.File;
-import java.util.Date;
 import org.bukkit.*;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.Configuration;
+import org.bukkit.command.*;
 
-public class BanList {
-    private static final File BANLIST = new File("banned-players.yml");
-    protected static final YamlConfiguration YAML = YamlConfiguration.loadConfiguration(BANLIST);
-    protected static Configuration defaults;
+public class Command_cookie {
     
-    public static void add(String name, String IP, String reason, String adminName) {
-        //methods here
-    }
 }
